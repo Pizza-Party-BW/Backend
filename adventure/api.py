e2c697f4f8f1a9ab0ec03f74dbefc51291ec7b22
justinelai <justine.lai@gmail.com>
2020-01-09 17:35:41 -0500

    adjust json response

--- adventure/api.py
+++ adventure/api.py
@@ -25,7 +25,7 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'id':room.id, 'x':room.x, 'y':room.y, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
 
 
 @csrf_exempt
@@ -60,10 +60,10 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'id':nextRoom.id, 'x':nextRoom.x, 'y':nextRoom.y, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'id':room.id, 'x':room.x, 'y':room.y, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 # ============ expose rooms model for rooms endpoint ============ 
@@ -73,7 +73,7 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = ('id','title', 'description', 'x', 'y', 'n_to', 's_to', 'e_to', 'w_to') 
         # eventually need to add id, x, y to rooms model
     
-class RoomViewSet(viewsets.ReadOnlyModelViewset):
+class RoomViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = RoomSerializer
     queryset = Room.objects.none()
 
