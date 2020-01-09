3612780ec1ab8858fb910d9c3c364031351853c2
Chris York <christopherayork1992@gmail.com>
2020-01-09 14:21:55 -0500

    oops

--- util/setup_world.py
+++ util/setup_world.py
@@ -3,8 +3,8 @@ from adventure.models import Player
 
 new_map = MapWorld()
 new_map.generate_map(10, 10)
-for x in range(0, new_map.grid):
-    for y in range(0, new_map.grid[x]):
+for x in range(0, len(new_map.grid)):
+    for y in range(0, len(new_map.grid[x])):
         room = new_map.grid[x][y]
         room.save()
 print(new_map)
